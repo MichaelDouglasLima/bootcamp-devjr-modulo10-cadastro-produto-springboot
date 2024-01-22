00f3d1841316642c2001661a7555b718b0d5cca3
Michael Douglas <michael.lima8@fatec.sp.gov.br>
2024-01-22 09:15:32 -0300

    Auto Increase Primary Key

--- src/main/java/com/abutua/productbackend/models/Category.java
+++ src/main/java/com/abutua/productbackend/models/Category.java
@@ -1,6 +1,8 @@
 package com.abutua.productbackend.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -11,6 +13,7 @@ public class Category {
     // Atributos
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String name;
