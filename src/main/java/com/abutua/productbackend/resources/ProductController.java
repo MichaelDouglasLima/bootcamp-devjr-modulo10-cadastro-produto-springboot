47c7a6c885ec79f9ff67104601ce3431490ee5ea
Michael Douglas <michael.lima8@fatec.sp.gov.br>
2024-01-25 15:45:14 -0300

    Update Entitys

--- src/main/java/com/abutua/productbackend/resources/ProductController.java
+++ src/main/java/com/abutua/productbackend/resources/ProductController.java
@@ -7,15 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.abutua.productbackend.models.Category;
 import com.abutua.productbackend.models.Product;
+import com.abutua.productbackend.repositories.CategoryRepository;
 import com.abutua.productbackend.repositories.ProductRepository;
 
 @RestController
@@ -24,6 +28,9 @@ public class ProductController {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @PostMapping("products")
     public ResponseEntity<Product> save(@RequestBody Product product) {
@@ -40,7 +47,7 @@ public class ProductController {
     }
 
     @GetMapping("products/{id}")
-    public ResponseEntity<Product> getProdut(@PathVariable int id) {
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {
         Product product = productRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
 
@@ -50,6 +57,42 @@ public class ProductController {
     @GetMapping("products")
     public List<Product> getProducts() {
         return productRepository.findAll();
+    }
+
+    @DeleteMapping("products/{id}")
+    public ResponseEntity<Void> removeProduct(@PathVariable int id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+
+        productRepository.delete(product);
+        
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("products/{id}")
+    public ResponseEntity<Void> updateProduct(@PathVariable int id, @RequestBody Product productUpdate) {
+
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+
+        if (productUpdate.getCategory() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Category can not be empty");
+        }
+        
+        Category category = categoryRepository.findById(productUpdate.getCategory().getId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+
+
+        product.setDescription(productUpdate.getDescription());
+        product.setName(productUpdate.getName());
+        product.setPrice(productUpdate.getPrice());
+        product.setNewProduct(productUpdate.isNewProduct());
+        product.setPromotion(productUpdate.isPromotion());
+        product.setCategory(category);
+
+        productRepository.save(product);
+        
+        return ResponseEntity.ok().build();
     }
 
 }
